722668c17296b9af9809984a4e6e5824b56b74a1
Reed Meher <reed.meher@gmail.com>
2022-12-29 13:16:22 -0600

    building working files

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function Footer() {
+  return (
+<>
+<p>Child Welfare Portal 2022</p>
+<a target="_blank" href="https://icons8.com/icon/31705/tree-planting">Tree Planting</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+</>
+  );
+}
+
+export default Footer;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <h2>this is the home page under construction</h2>
+        </>
+    );
+};
+
+export default Home;
