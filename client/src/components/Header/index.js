096bbaa09d4d52cb0d6d5712b400603249ce83ff
Reed Meher <reed.meher@gmail.com>
2023-01-02 12:39:49 -0600

    building homepage and theme

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,7 +12,7 @@ function Header() {
         <>
           <Navbar bg="white" variant="light" className="mb-3 p-3" expand="md">
             <Container fluid>
-              <Navbar.Brand href="#" className="">Child Welfare Portal</Navbar.Brand>
+              <Navbar.Brand href="#" className="">Public Child Welfare Portal</Navbar.Brand>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md}`}  />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-md}`}
@@ -20,41 +20,42 @@ function Header() {
                 placement="end"
               >
                 <Offcanvas.Header closeButton >
-                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md}`} >
-                    Child Welfare Portal
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md}`}>
+                    Public Child Welfare Portal
                   </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                   <Nav className="justify-content-end flex-grow-1 pe-3">
                     <Nav.Link href="#action1">Dashboard</Nav.Link>
                     <Nav.Link href="#action2">Login</Nav.Link>
-                    <Nav.Link href="#action2">Donate</Nav.Link>
+                    <Nav.Link href="#action3">Contact</Nav.Link>
+                    <Nav.Link href="#action3">Donate</Nav.Link>
                     <NavDropdown
                       title="Resources"
                       id={`offcanvasNavbarDropdown-expand-md}`}
                     >
                       <NavDropdown.Item href="#action3">ICWA</NavDropdown.Item>
                       <NavDropdown.Item href="#action4">Social Work Code of Ethics</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Data Privacy</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">History</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Advocacy</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Domestic Violence</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Abuse</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Substance Use Disorder</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Education Neglect and Truancy</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">The Court System & CHIPS</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Child Protection & the Law</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Incarceration</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Mental Health</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Parenting Resources</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Case Planning</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Interviewing</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Foster Care in MN</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">SSIS & Data Systems</NavDropdown.Item>
-                      <NavDropdown.Item href="#action3">Title IV-E</NavDropdown.Item>
+                      <NavDropdown.Item href="#action5">Data Privacy</NavDropdown.Item>
+                      <NavDropdown.Item href="#action6">History</NavDropdown.Item>
+                      <NavDropdown.Item href="#action7">Advocacy</NavDropdown.Item>
+                      <NavDropdown.Item href="#action8">Domestic Violence</NavDropdown.Item>
+                      <NavDropdown.Item href="#action9">Abuse</NavDropdown.Item>
+                      <NavDropdown.Item href="#action10">Substance Use Disorder</NavDropdown.Item>
+                      <NavDropdown.Item href="#action11">Education Neglect and Truancy</NavDropdown.Item>
+                      <NavDropdown.Item href="#action12">The Court System & CHIPS</NavDropdown.Item>
+                      <NavDropdown.Item href="#action13">Child Protection & the Law</NavDropdown.Item>
+                      <NavDropdown.Item href="#action14">Incarceration</NavDropdown.Item>
+                      <NavDropdown.Item href="#action15">Mental Health</NavDropdown.Item>
+                      <NavDropdown.Item href="#action16">Parenting Resources</NavDropdown.Item>
+                      <NavDropdown.Item href="#action17">Case Planning</NavDropdown.Item>
+                      <NavDropdown.Item href="#action18">Interviewing</NavDropdown.Item>
+                      <NavDropdown.Item href="#action19">Foster Care in MN</NavDropdown.Item>
+                      <NavDropdown.Item href="#action20">SSIS & Data Systems</NavDropdown.Item>
+                      <NavDropdown.Item href="#action21">Title IV-E</NavDropdown.Item>
                       {/* <NavDropdown.Divider />
-                      <NavDropdown.Item href="#action5">
-                        Something else here
+                      <NavDropdown.Item href="#action22">
+                        something added?
                       </NavDropdown.Item> */}
                     </NavDropdown>
                   </Nav>
@@ -65,7 +66,7 @@ function Header() {
                       className="me-2"
                       aria-label="Search"
                     />
-                    <Button variant="outline-success">Search</Button>
+                    <Button className="search">Search</Button>
                   </Form>
                 </Offcanvas.Body>
               </Navbar.Offcanvas>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,9 @@ import Container from 'react-bootstrap/Container';
 const Home = () => {
     return (
         <>
-        <Container fluid className="container-sm ">
-        <h1>Welcome!</h1>
-        <p>CWRP is a site dedicated to child welfare workers in MN. It is a hub for resources commonly needed by new and experience CW workers. Users are also able to create accounts so that they can save favorite resource links and created simple, learning to-do lists.</p>
+        <Container fluid className="text-center p-5 mt-5 box">
+        <h1>Welcome to the Public Child Welfare Portal</h1>
+        <p className="mainText">This is a site dedicated to child welfare workers in MN. It is a hub for resources commonly needed by new and experience CW workers. Users are also able to create accounts so that they can save favorite resource links and created simple, learning to-do lists.</p>
         </Container>
         </>
     );
