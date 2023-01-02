b47de9f025766047ca834d465765c42868b29c0b
Reed Meher <reed.meher@gmail.com>
2023-01-02 12:56:32 -0600

    building frontend routes

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,13 +6,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <>
           <Navbar bg="white" variant="light" className="mb-3 p-3" expand="md">
             <Container fluid>
-              <Navbar.Brand href="#" className="">Public Child Welfare Portal</Navbar.Brand>
+              <Navbar.Brand as={Link} to="/" className="">Public Child Welfare Portal</Navbar.Brand>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md}`}  />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-md}`}
@@ -26,38 +27,38 @@ function Header() {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                   <Nav className="justify-content-end flex-grow-1 pe-3">
-                    <Nav.Link href="#action1">Dashboard</Nav.Link>
-                    <Nav.Link href="#action2">Login</Nav.Link>
-                    <Nav.Link href="#action3">Contact</Nav.Link>
-                    <Nav.Link href="#action3">Donate</Nav.Link>
                     <NavDropdown
                       title="Resources"
                       id={`offcanvasNavbarDropdown-expand-md}`}
                     >
-                      <NavDropdown.Item href="#action3">ICWA</NavDropdown.Item>
-                      <NavDropdown.Item href="#action4">Social Work Code of Ethics</NavDropdown.Item>
-                      <NavDropdown.Item href="#action5">Data Privacy</NavDropdown.Item>
-                      <NavDropdown.Item href="#action6">History</NavDropdown.Item>
-                      <NavDropdown.Item href="#action7">Advocacy</NavDropdown.Item>
-                      <NavDropdown.Item href="#action8">Domestic Violence</NavDropdown.Item>
-                      <NavDropdown.Item href="#action9">Abuse</NavDropdown.Item>
-                      <NavDropdown.Item href="#action10">Substance Use Disorder</NavDropdown.Item>
-                      <NavDropdown.Item href="#action11">Education Neglect and Truancy</NavDropdown.Item>
-                      <NavDropdown.Item href="#action12">The Court System & CHIPS</NavDropdown.Item>
-                      <NavDropdown.Item href="#action13">Child Protection & the Law</NavDropdown.Item>
-                      <NavDropdown.Item href="#action14">Incarceration</NavDropdown.Item>
-                      <NavDropdown.Item href="#action15">Mental Health</NavDropdown.Item>
-                      <NavDropdown.Item href="#action16">Parenting Resources</NavDropdown.Item>
-                      <NavDropdown.Item href="#action17">Case Planning</NavDropdown.Item>
-                      <NavDropdown.Item href="#action18">Interviewing</NavDropdown.Item>
-                      <NavDropdown.Item href="#action19">Foster Care in MN</NavDropdown.Item>
-                      <NavDropdown.Item href="#action20">SSIS & Data Systems</NavDropdown.Item>
-                      <NavDropdown.Item href="#action21">Title IV-E</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/icwa">ICWA</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/ethics">Social Work Code of Ethics</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/dataprivacy">Data Privacy</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/history">History</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/advocacy">Advocacy</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/dv">Domestic Violence</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/abuse">Abuse</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/sud">Substance Use Disorder</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/edneglect">Education Neglect and Truancy</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/court">The Court System & CHIPS</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/cplaw">Child Protection & the Law</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/incarceration">Incarceration</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/mentalhealth">Mental Health</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/parenting">Parenting Resources</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/caseplanning">Case Planning</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/interviewing">Interviewing</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/fostercare">Foster Care in MN</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/ssis">SSIS & Data Systems</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/iv-e">Title IV-E</NavDropdown.Item>
                       {/* <NavDropdown.Divider />
                       <NavDropdown.Item href="#action22">
                         something added?
                       </NavDropdown.Item> */}
                     </NavDropdown>
+                    <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    <Nav.Link as={Link} to="/donate">Donate</Nav.Link>
                   </Nav>
                   <Form className="d-flex">
                     <Form.Control
