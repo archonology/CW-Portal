b34d59de3efa12bb3f6c4abe76bc04a6d1c93c59
Reed Meher <reed.meher@gmail.com>
2023-01-02 12:05:28 -0600

    building root colors

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
 
 const Home = () => {
     return (
         <>
-        <h2>this is the home page under construction</h2>
+        <Container fluid className="container-sm ">
+        <h1>Welcome!</h1>
+        <p>CWRP is a site dedicated to child welfare workers in MN. It is a hub for resources commonly needed by new and experience CW workers. Users are also able to create accounts so that they can save favorite resource links and created simple, learning to-do lists.</p>
+        </Container>
         </>
     );
 };
