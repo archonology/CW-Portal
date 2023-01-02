370dfda0a7edd12bf193851de8cb0ca1760aa5c7
Reed Meher <reed.meher@gmail.com>
2023-01-02 11:47:56 -0600

    building navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,36 +10,52 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 function Header() {
     return (
         <>
-          <Navbar bg="white" className="mb-3 p-3" expand="md">
+          <Navbar bg="white" variant="light" className="mb-3 p-3" expand="md">
             <Container fluid>
-              <Navbar.Brand href="#">Child Welfare Portal</Navbar.Brand>
-              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md}`} />
+              <Navbar.Brand href="#" className="">Child Welfare Portal</Navbar.Brand>
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md}`}  />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-md}`}
                 aria-labelledby={`offcanvasNavbarLabel-expand-md}`}
                 placement="end"
               >
-                <Offcanvas.Header closeButton>
-                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md}`}>
-                    Offcanvas
+                <Offcanvas.Header closeButton >
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md}`} >
+                    Child Welfare Portal
                   </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                   <Nav className="justify-content-end flex-grow-1 pe-3">
-                    <Nav.Link href="#action1">Home</Nav.Link>
-                    <Nav.Link href="#action2">Link</Nav.Link>
+                    <Nav.Link href="#action1">Dashboard</Nav.Link>
+                    <Nav.Link href="#action2">Login</Nav.Link>
+                    <Nav.Link href="#action2">Donate</Nav.Link>
                     <NavDropdown
-                      title="Dropdown"
+                      title="Resources"
                       id={`offcanvasNavbarDropdown-expand-md}`}
                     >
-                      <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                      <NavDropdown.Item href="#action4">
-                        Another action
-                      </NavDropdown.Item>
-                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action3">ICWA</NavDropdown.Item>
+                      <NavDropdown.Item href="#action4">Social Work Code of Ethics</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Data Privacy</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">History</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Advocacy</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Domestic Violence</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Abuse</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Substance Use Disorder</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Education Neglect and Truancy</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">The Court System & CHIPS</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Child Protection & the Law</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Incarceration</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Mental Health</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Parenting Resources</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Case Planning</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Interviewing</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Foster Care in MN</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">SSIS & Data Systems</NavDropdown.Item>
+                      <NavDropdown.Item href="#action3">Title IV-E</NavDropdown.Item>
+                      {/* <NavDropdown.Divider />
                       <NavDropdown.Item href="#action5">
                         Something else here
-                      </NavDropdown.Item>
+                      </NavDropdown.Item> */}
                     </NavDropdown>
                   </Nav>
                   <Form className="d-flex">
