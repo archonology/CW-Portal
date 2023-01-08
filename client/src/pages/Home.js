d9842a6dad9a2da33dc46ea6bd891b5cbc7bd179
Reed Meher <reed.meher@gmail.com>
2023-01-08 14:04:32 -0600

    card template moved to components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,49 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
-
+import ResourceCard from "../components/Cards";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import { Button, IconButton } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 const Home = () => {
-    return (
-        <>
-        <Container  className="text-center p-5 mt-5 box">
+  return (
+    <>
+      <Container className="text-center p-5 mt-5 box">
         <h1>Welcome to the Public Child Welfare Portal</h1>
         <p className="mainText">This is a site dedicated to child welfare workers in MN. It is a hub for resources commonly needed by new and experience CW workers. Users are also able to create accounts so that they can save favorite resource links and created simple, learning to-do lists.</p>
-        </Container>
-        
-        <Card sx={{ maxWidth: 345, margin: 10, boxShadow: 10 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={"https://capacity.childwelfare.gov/sites/default/files/styles/hero_desktop/public/media-image/GettyImages-1084390966-1920px%20-%20mirror.jpg?h=bf8618f3&itok=XH04zm0t"}
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Child Welfare Capacity Building Collaborative
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        The Center for Tribes collaborates with American Indian and Alaska Native nations to help strengthen Tribal child and family systems and services in order to nurture the safety, permanency, and well-being of children, youth, and families.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Save to a List</Button>
-        <Button size="small">Visit the Site</Button>
-      </CardActions>
-    </Card>
+      </Container>
 
-        </>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{}}
+      >
 
-    );
+        <Box sx={{ flexGrow: 1 }} alignContent="center">
+          <Grid container spacing={1}>
+
+            <ResourceCard />
+
+          </Grid>
+        </Box>
+      </Grid>
+
+    </>
+
+  );
 };
 
 export default Home;
