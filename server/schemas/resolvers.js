27b3a130a9ee47e0311b262c8daa3c33ebca0273
Reed Meher <reed.meher@gmail.com>
2023-01-10 10:30:35 -0600

    building resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,71 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { Admin, User, Topic, Resource } = require('../models');
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select("-__v -password");
+                return userData;
+            };
+
+            if (context.admin) {
+                const adminData = await Admin.findOne({ _id: context.user._id })
+                    .select("-__v -password");
+                return adminData;
+            }
+            throw new AuthenticationError("Please login or sign up to continue.");
+        },
+    },
+
+    Mutatation: {
+
+        createAdmin: async (parent, { username, email, password }) => {
+            const admin = await Admin.create({ username, email, password });
+            const token = signToken(admin);
+            return { token, admin };
+        },
+
+        createUser: async (parent, { username, email, password }) => {
+            const user = await Admin.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        loginAdmin: async (parent, { email, password }) => {
+            const admin = await Admin.findOne({ email });
+            if (!admin) {
+                throw new AuthenticationError('No admin found by that email address.');
+            }
+
+            const correctPassword = await admin.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Password is incorrect.');
+            }
+
+            const token = signToken(admin);
+            return { token, admin };
+        },
+
+        loginUser: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found by that email address.');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Password is incorrect.');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+    }
+}
