cea95e4d5cfd7a39c0ae0bcac3bdadc1f7706c3b
Reed Meher <reed.meher@gmail.com>
2023-01-12 00:41:35 -0600

    building mutations on frontend

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
-const resourceSchema = require("./Resource");
 
 
 // define user schema
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -45,6 +45,7 @@ const typeDefs = gql`
     type Subtopic {
         _id: ID
         title: String
+        url: String
         text: String
         resources: [Resource]
     }
@@ -63,16 +64,17 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        createAdmin(username: String!, email: String!, password: String!): Auth
+        createAdmin(username: String!, email: String!, password: String!): Admin
         createUser(username: String!, email: String!, password: String!): Auth
         loginAdmin(email: String!, password: String!): Admin
         loginUser(email: String!, password: String!): User
-        createTopic(topic: String!, url: String! text: String!, image: String!): Admin
-        createResource(title: String!, url: String!, text: String!, image: String!, link: String!): Admin            
-        addResourceToTopic(_id: ID!, title: String!, url: String!, text: String!, image: String!, link: String!): Admin
-        adminUpdateTopic(_id: ID!, topic: String!, text: String!, image: String!): Admin
-        adminUpdateResource(_id: ID!, title: String!, url: String!, text: String!, image: String!, link: String!): Admin        
-        adminDeleteTopic(_id: ID!): Admin
+        createTopic(topic: String!, url: String! text: String!, image: String!): Topic
+        createSubtopic(title: String!, url: String! text: String!): Topic
+        createResource(title: String!, url: String!, text: String!, image: String!, link: String!): Resource            
+        addResourceToTopic(_id: ID!, title: String!, url: String!, text: String!, image: String!, link: String!): Topic
+        adminUpdateTopic(_id: ID!, topic: String!, text: String!, image: String!): Topic
+        adminUpdateResource(_id: ID!, title: String!, url: String!, text: String!, image: String!, link: String!): Resource        
+        adminDeleteTopic(_id: ID!): Topic
         adminDeleteResource(_id: ID!): Admin
         userAddResource(_id: ID!, title: String!, url: String!, text: String!, image: String!, link: String!): User
         userDeleteResource(_id: ID!): User
