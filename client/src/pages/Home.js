54ef7cdc6ad1952ac4cf1339ebd2b97aabce37eb
Reed Meher <reed.meher@gmail.com>
2023-02-02 09:14:50 -0600

    posts section added to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,11 @@ import Resources from "../components/Resources";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Image from 'mui-image';
+import sample from '../media/DNGF7977.jpeg'
+
 
 const Home = () => {
   return (
@@ -13,8 +18,17 @@ const Home = () => {
         <p className="mainText">This is a site dedicated to child welfare workers in MN. It is a hub for resources commonly needed by new and experience CW workers. Users are also able to create accounts so that they can save favorite resource links and created simple, learning to-do lists.</p>
       </Container>
 
+      <Container fluid className="bg-dark p-1">
+        <Stack direction="row" spacing={5} margin={3}>
+          <Image src={sample} fit="contain"></Image>
+          <p>This is a site dedicated to child welfare workers in MN. It is a hub for resources commonly needed by new and experience CW workers. Users are also able to create accounts so that they can save favorite resource links and created simple, learning to-do lists.<br></br><span><Button variant="outlined" color="success" sx={{ marginTop: 3}} size="small">Learn More</Button></span></p>
+          <Stack spacing={2} direction="row">
+            
+          </Stack>
+        </Stack>
 
-  
+      </Container>
+
     </>
 
   );
